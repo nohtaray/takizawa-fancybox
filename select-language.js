dff70c327d77ff80eba7b3e8bf8b990dde617d8c
Sunshineddy <sunshine.akira.0215@gmail.com>
2017-08-06 09:54:34 +0900

    debug_eventchange2

--- select-language.js
+++ select-language.js
@@ -4,30 +4,6 @@ $(function() {
     return;
   }
 
-  var event = document.getElementById('select_lang');
-  console.log(event);
-  event.onchange = function() { // セレクトボックスに変更が加えられたら
-    var index = document.select_form.select_lang.selectedIndex;
-    var val = document.select_form.select_lang.options[index].value;
-    console.log(val);
-
-    if(index != 0) {
-      changeLanguage(val);
-      console.log("OK");
-    }
-  }
-
-  $('#modal-content li a').click(function() {
-    var lang = $(this).data('lang');
-    changeLanguage(lang);
-  });
-
-  var lang = Cookies.get('lang');
-  if (lang) {
-    changeLanguage(lang);
-    return;
-  }
-
   var modalHtml = ' \
 <a class="fancybox" href="#modal-content"></a> \
 <div id="modal-content" style="display:none; width: 60%"> \
@@ -42,6 +18,30 @@ $(function() {
   $(document.body).append(modalHtml);
 
   $('.fancybox').fancybox({}).click();
+
+  $('#modal-content li a').click(function() {
+    var lang = $(this).data('lang');
+    changeLanguage(lang);
+  });
+
+  var event = document.getElementById('select_lang');
+  console.log(event);
+  event.onchange = function() { // セレクトボックスに変更が加えられたら
+    var index = document.select_form.select_lang.selectedIndex;
+    var val = document.select_form.select_lang.options[index].value;
+    console.log(val);
+
+    if(index != 0) {
+      changeLanguage(val);
+      console.log("OK");
+    }
+  }
+
+  var lang = Cookies.get('lang');
+  if (lang) {
+    changeLanguage(lang);
+    return;
+  }
 
   function changeLanguage(lang) {
     Cookies.set('lang', lang);
