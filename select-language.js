21a152c0daa148d0e4f8565cd97b515921642249
Sunshineddy <sunshine.akira.0215@gmail.com>
2017-08-06 10:02:06 +0900

    debug_eventchange3

--- select-language.js
+++ select-language.js
@@ -4,6 +4,25 @@ $(function() {
     return;
   }
 
+  var event = document.getElementById('select_lang');
+  console.log(event);
+  event.onchange = function() { // セレクトボックスに変更が加えられたら
+    var index = document.select_form.select_lang.selectedIndex;
+    var val = document.select_form.select_lang.options[index].value;
+    console.log(val);
+
+    if(index != 0) {
+      changeLanguage(val);
+      console.log("OK");
+    }
+  }
+
+  var lang = Cookies.get('lang');
+  if (lang) {
+    changeLanguage(lang);
+    return;
+  }
+
   var modalHtml = ' \
 <a class="fancybox" href="#modal-content"></a> \
 <div id="modal-content" style="display:none; width: 60%"> \
@@ -23,25 +42,6 @@ $(function() {
     var lang = $(this).data('lang');
     changeLanguage(lang);
   });
-
-  var event = document.getElementById('select_lang');
-  console.log(event);
-  event.onchange = function() { // セレクトボックスに変更が加えられたら
-    var index = document.select_form.select_lang.selectedIndex;
-    var val = document.select_form.select_lang.options[index].value;
-    console.log(val);
-
-    if(index != 0) {
-      changeLanguage(val);
-      console.log("OK");
-    }
-  }
-
-  var lang = Cookies.get('lang');
-  if (lang) {
-    changeLanguage(lang);
-    return;
-  }
 
   function changeLanguage(lang) {
     Cookies.set('lang', lang);
